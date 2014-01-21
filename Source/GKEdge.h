3b897a37ce8862aa1dd12c61a34dd2b55182067b
Sam Deane <sam@elegantchaos.com>
2014-01-21 10:03:48 +0000

    Updated copyrights in code.

--- Source/GKEdge.h
+++ Source/GKEdge.h
@@ -1,5 +1,5 @@
 //  Created by Pieter Omvlee on 19/09/2011.
-//  Copyright (c) 2011 Bohemian Coding. All rights reserved.
+//  Copyright (c) 2014 Bohemian Coding. All rights reserved.
 
 #import "GKObjectEnum.h"
 
--- Source/GKRect+Axis.m
+++ Source/GKRect+Axis.m
@@ -1,5 +1,5 @@
 //  Created by Pieter Omvlee on 20/09/2011.
-//  Copyright (c) 2011 Bohemian Coding. All rights reserved.
+//  Copyright (c) 2014 Bohemian Coding. All rights reserved.
 
 #import "GKRect+Axis.h"
 #import "GKRect+Edge.h"
